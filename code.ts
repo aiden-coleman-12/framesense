ad6b96e98f1834dc2066ed2ec0cec2c1021f2985
Aiden Coleman <aidencoleman@Aidens-MacBook-Air.local>
2026-02-02 17:11:21 -0500

    logo

--- code.ts
+++ code.ts
@@ -23,7 +23,7 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
     for (let i = 0; i < numberOfRectangles; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+      rect.fills = [{ type: 'SOLID', color: { r: 0, g: 1, b: 0 } }];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
